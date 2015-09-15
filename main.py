191a073081bf5e2d98346011e803cfef04255472
dagrooms52 <dagn9c@mst.edu>
2015-09-14 23:33:37 -0500

    Drawing graphics on View screen
    Integrated graphics drawing on view screen. Still need to set image
    height proportional to application window height. Selects any image
    number from input string to reduce duplicate code.

--- main.py
+++ main.py
@@ -71,8 +71,8 @@ class TitleScreen(Screen):
 class CreateScreen(Screen):
     def on_enter(self):
         print('Enter create screen')
-        
-class ViewScreen(Screen):    
+
+class ViewScreen(Screen):
     loadfile = ObjectProperty(None)
     savefile = ObjectProperty(None)
 
@@ -89,74 +89,73 @@ class ViewScreen(Screen):
         #loads the file
         tab = open(os.path.join(path, filename[0]))
         self._popup.dismiss()
-        wid = Widget(size_hint_x=50)
-        #draws the tab on the screen
-        self.drawtab(wid, tab)
+
+        # Draw the tab on the screen
+        wid = Widget(size_hint=(10, 1), pos=(0.5, 0))
+        self.drawtab(tab, wid)
+
+        slide = Slider(min=0, max=1, value=25, orientation='horizontal', step=0.01, size_hint=(1, 0.1))
+        slide.bind(value=partial(self.scroll_change, slide))
+
+        scrl = ScrollView()
+        scrl.bind(scroll_x=partial(self.slider_change, slide))
+        scrl.add_widget(wid)
+
+        self.add_widget(slide)
+        self.add_widget(scrl)
 
     def show_save(self):
         content = SaveDialog(save=self.save, cancel=self.dismiss_popup)
         print(self.save)
         self._popup = Popup(title="Save file", content=content, size_hint=(0.9, 0.9))
-        self._popup.open()  
+        self._popup.open()
 
-    # Need label widget for side-scrolling
-    def drawtab(self, wid, tab):
-        # Should open a file explorer here to get tab file, check extension
+    def drawtab(self, tab, wid):
         charwidth = 32
         charheight = 32
         lineheight = 32
-        startheight = 200
+        startheight = 500
         i = 0
 
-        #atlas = Atlas(r"C:\Users\Daniel\Desktop\Classes\SeniorDesign\Code\Assets\main.atlas")
-        for line in tab:
-            print(line)
+        # Get path to current file to find atlas file
+        curdir = os.path.dirname(os.path.realpath(__file__))
+        #test print
+        print(curdir + r"/Assets/main.atlas")
+        atlas = Atlas(curdir + r"/Assets/main.atlas")
 
+        for line in tab:
             # Parse by character, translating input to graphical output
             with wid.canvas:
                 for j in range(len(line)):
-                    print(line[j])
-                    if j == 0:
+                    thischar = line[j]
+                    print(thischar)
+                    if thischar == '\n' or thischar == ':':
+                        pass
+                    elif j == 0:
                         # T bar
                         Rectangle(source='atlas://Assets/main/tbar',
                                   pos=(0, startheight-(i*lineheight)),
                                   size=(32, 32))
-                    elif line[j] == "|":
-                        print("inside if plus bar")
+                    elif thischar == "|" and j > 0:
                         # Plus bar
                         print("Making vertical line")
                         Rectangle(source='atlas://Assets/main/plusbar',
                                   pos=(j*charwidth, startheight-(i*lineheight)),
                                   size=(32, 32))
-                    elif line[j] == "-":
+                    elif thischar == "-":
                         # Horizontal line
                         print("Making horizontal line")
                         Rectangle(source='atlas://Assets/main/bar',
                                   pos=(j*charwidth, startheight-(i*lineheight)),
                                   size=(32, 32))
-                    elif line[j] == '0':
-                        print("Making 0")
-                        Rectangle(source='atlas://Assets/main/normzero',
-                                  pos=(j*charwidth, startheight-(i*lineheight)),
-                                  size=(32, 32))
-                    elif line[j] == '1':
-                        print("Making 1")
-                        Rectangle(source='atlas://Assets/main/normone',
-                                  pos=(j*charwidth, startheight-(i*lineheight)),
-                                  size=(32, 32))
-                    elif line[j] == '2':
-                        print("Making 2")
-                        Rectangle(source='atlas://Assets/main/normtwo',
-                                  pos=(j*charwidth, startheight-(i*lineheight)),
-                                  size=(32, 32))
-                    elif line[j] == '3':
-                        print("Making 3")
-                        Rectangle(source='atlas://Assets/main/normthree',
+                    elif int(thischar) >= 0 and int(thischar) <= 24:
+                        print("Making " + thischar)
+                        Rectangle(source=('atlas://Assets/main/norm' + thischar),
                                   pos=(j*charwidth, startheight-(i*lineheight)),
                                   size=(32, 32))
                     else:
                         print("Can't draw it yet")
-                   
+
             # Increment counter
             i += 1
 
@@ -165,48 +164,48 @@ class ViewScreen(Screen):
 
     def slider_change(self, slide, instance, value):
         if value >= 0:
-            slide.value = value       
+            slide.value = value
 
 #main widget of the app
 class TabMachine(BoxLayout):
     def __init__(self, **kwargs):
         super(TabMachine, self).__init__(**kwargs)
         self.orientation = 'vertical'
-        #adds the screen manager to the main app
+        # Adds the screen manager to the main app
         self.content = ScreenManager()
         self.content.add_widget(TitleScreen(name='Title'))
         self.content.add_widget(CreateScreen(name='CreateScreen'))
-        self.content.add_widget(ViewScreen(name="ViewScreen"))
-        #displays in the order of adds
-        #add the nav bar to the top
+        self.content.add_widget(ViewScreen(name='ViewScreen'))
+        # Displays in the order of adds
+        # Add the nav bar to the top
         self.slide_menu = NavMenu(root=self)
         self.add_widget(self.slide_menu)
-        #add the screen to the middle.
+        # Add the screen to the middle.
         self.add_widget(self.content)
-  
+
     def get_current_slide(self):
-        print 'test1'
+        print('test1')
         #return self.content.current
-    
+
     def set_current_slide(self, jump_to):
         self.content.current = jump_to
 
 Builder.load_file("navmenu.kv")
 class NavMenu(BoxLayout):
     slide_spinner = ObjectProperty(None)
-    
+
     def __init__(self, root, **kwargs):
         super(NavMenu, self).__init__(**kwargs)
         self.root = root
         #self.slide_spinner.values = screens
-        
+
     #def go_slide(self, spinner):
     #    if spinner.text in screens:
     #        self.root.set_current_slide(spinner.text)
-       
+
     def go_create(self):
         self.root.set_current_slide('CreateScreen')
-        
+
     def go_view(self):
         self.root.set_current_slide('ViewScreen')
 
@@ -215,6 +214,6 @@ class TabMachineApp(App):
 
     def build(self):
         return TabMachine()
-    
+
 if __name__ == '__main__':
     TabMachineApp().run()
